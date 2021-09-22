05ea68f3b3e06290c64b4b6204497c1eeb0512d7
Subal Pant <email@example.com>
2021-09-21 19:44:07 -0700

    small bug fixes

--- server/handlers/hello.go
+++ server/handlers/hello.go
@@ -8,7 +8,7 @@ import (
 
 func (h *Handler) Hello(c echo.Context) error {
 	// hello world :)
-	res := map[string]string{"msg": "hello world :)"}
+	res := map[string]string{"msg": "hello world"}
 
 	return c.JSON(http.StatusOK, res)
 }
--- server/server/auth.go
+++ server/server/auth.go
@@ -65,14 +65,14 @@ func getPemCert(token *jwt.Token) (string, error) {
 		return cert, err
 	}
 
-	for k, _ := range jwks.Keys {
+	for k := range jwks.Keys {
 		if token.Header["kid"] == jwks.Keys[k].Kid {
 			cert = "-----BEGIN CERTIFICATE-----\n" + jwks.Keys[k].X5c[0] + "\n-----END CERTIFICATE-----"
 		}
 	}
 
 	if cert == "" {
-		err := errors.New("Unable to find appropriate key.")
+		err := errors.New("unable to find appropriate key")
 		return cert, err
 	}
 
